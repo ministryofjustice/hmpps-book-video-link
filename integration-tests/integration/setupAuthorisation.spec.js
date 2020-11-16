12b82234af54d5dfd387047ccdf7041b82348502
G <gurnank@outlook.com>
2020-11-16 14:31:42 +0000

    DCS-675 removed unnecessary cypress tests

--- integration-tests/integration/setupAuthorisation.spec.js
+++ integration-tests/integration/setupAuthorisation.spec.js
@@ -3,11 +3,8 @@ const CourtVideoLinkHomePage = require('../pages/videolink/courtVideoLinkHomePag
 const LoginPage = require('../pages/loginPage')
 
 context('A user can book an appointment if has all the required roles', () => {
-  before(() => {
-    cy.clearCookies()
-  })
-
   beforeEach(() => {
+    cy.clearCookies()
     cy.task('reset')
     cy.task('stubLoginCourt')
     cy.task('stubUserMe')
@@ -19,30 +16,9 @@ context('A user can book an appointment if has all the required roles', () => {
     CourtVideoLinkHomePage.verifyOnPage()
   })
 
-  it('should continue to the Book a video link with a prison page becuase user has MORE than ALL the required roles', () => {
-    cy.task('stubUserMeRoles', [
-      { roleCode: 'VIDEO_LINK_COURT_USER' },
-      { roleCode: 'GLOBAL_SEARCH' },
-      { roleCode: 'SOME_OTHER_ROLE' },
-    ])
-    cy.login()
-    CourtVideoLinkHomePage.verifyOnPage()
-  })
-  it('should redirect to no-service-access page because user has only ONE of the required roles', () => {
-    cy.task('stubUserMeRoles', [{ roleCode: 'GLOBAL_SEARCH' }])
-    cy.login()
-    NoServiceAccessPage.verifyOnPage()
-  })
-
   it('should redirect to no-service-access page because user has NONE of the required roles', () => {
     cy.task('stubUserMeRoles', [])
     cy.login()
-    NoServiceAccessPage.verifyOnPage()
-  })
-
-  it('should go to the login page when clicking Exit link', () => {
-    cy.login()
-    cy.visit('/no-service-access')
     const noServiceAccessPage = NoServiceAccessPage.verifyOnPage()
     noServiceAccessPage.exitLink().click()
     LoginPage.verifyOnPage()
