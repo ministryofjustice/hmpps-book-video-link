d6c1b146449ddbb52b3b1505fe28617c4a4cb9c8
Louise Norris <louise.norris@digital.justice.gov.uk>
2020-11-20 16:18:05 +0000

    LNorris DCS-712 implemented global error handling

--- backend/tests/selectCourtAppointmentRooms.test.js
+++ backend/tests/selectCourtAppointmentRooms.test.js
@@ -1,9 +1,6 @@
 const { selectCourtAppointmentRoomsFactory } = require('../controllers/appointments/selectCourtAppointmentRooms')
 const { notifyClient } = require('../shared/notifyClient')
-const errorHandler = require('../middleware/errorHandler')
 const config = require('../config')
-
-jest.mock('../middleware/errorHandler')
 
 describe('Select court appointment rooms', () => {
   const prisonApi = {}
@@ -140,14 +137,7 @@ describe('Select court appointment rooms', () => {
 
       req.flash.mockImplementation(() => [])
 
-      await index(req, res)
-
-      expect(errorHandler).toHaveBeenCalledWith(
-        req,
-        res,
-        new Error('Appointment details are missing'),
-        '/MDI/offenders/A12345/add-appointment'
-      )
+      await expect(index(req, res)).rejects.toThrow('Appointment details are missing')
     })
 
     it('should call getAvailableLocationsForVLB with the correct parameters', async () => {
@@ -280,14 +270,7 @@ describe('Select court appointment rooms', () => {
 
       req.flash.mockImplementation(() => [])
 
-      await validateInput(req, res)
-
-      expect(errorHandler).toHaveBeenCalledWith(
-        req,
-        res,
-        new Error('Appointment details are missing'),
-        '/MDI/offenders/A12345/add-appointment'
-      )
+      await expect(validateInput(req, res)).rejects.toThrow('Appointment details are missing')
     })
 
     it('should pack appointment details back into flash before rendering', async () => {
