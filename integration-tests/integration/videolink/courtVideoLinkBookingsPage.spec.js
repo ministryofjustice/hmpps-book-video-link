27d49217ef4d0cb54e5ec7b178f06ea996521982
Louise Norris <louise.norris@digital.justice.gov.uk>
2020-11-24 16:16:28 +0000

    LNorris DCS-707 amended date format for the date input on /bookings page to match the h1 heading format

--- integration-tests/integration/videolink/courtVideoLinkBookingsPage.spec.js
+++ integration-tests/integration/videolink/courtVideoLinkBookingsPage.spec.js
@@ -120,6 +120,7 @@ context('A user can view the video link home page', () => {
   it('Returns unsupported courts when Other is selected', () => {
     cy.visit('/bookings')
     const courtVideoBookingsPage = CourtVideoLinkBookingsPage.verifyOnPage()
+    courtVideoBookingsPage.dateInput().should('have.value', Cypress.moment().format('D MMMM YYYY'))
     courtVideoBookingsPage.courtOption().select('Other')
     courtVideoBookingsPage.submitButton().click()
 
--- static/js/main.js
+++ static/js/main.js
@@ -1,21 +1,21 @@
-$(document).ready(function() {
-  $('.date-input').each(function(index, element) {
+$(document).ready(function () {
+  $('.date-input').each(function (index, element) {
     const disableFutureDates = Boolean($(element).data('disable-future-date'))
     const disablePastDates = Boolean($(element).data('disable-past-date'))
     const maxDate = disableFutureDates ? '0' : undefined
     const minDate = $(element).data('min-date') ? $(element).data('min-date') : disablePastDates ? '0' : undefined
-    const dateFormat = 'dd/mm/yy'
+    const dateFormat = $(element).attr('date-format') || 'dd/mm/yy'
 
     $(element).datepicker({
       dateFormat,
-      showOtherMonths: true,
-      selectOtherMonths: true,
+      showOtherMonths: false,
+      selectOtherMonths: false,
       maxDate: maxDate,
       minDate: minDate,
     })
   })
 
-  $('.js-prisoner-search-clear-alerts').click(function(e) {
+  $('.js-prisoner-search-clear-alerts').click(function (e) {
     e.preventDefault()
     $('.js-prisoner-search-alerts input[type=checkbox]').prop('checked', false)
   })
