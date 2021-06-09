9e3cb2d387097083ddf19b671288cdb8d9d75173
GCheema <50441412+GurnankCheema@users.noreply.github.com>
2021-06-09 09:14:29 +0100

    DCS-999 homepage has 'Manage your list of courts button' (#220)
    DCS-999 homepage has 'Manage your list of courts button'

--- integration-tests/integration/homePage.spec.js
+++ integration-tests/integration/homePage.spec.js
@@ -1,10 +1,34 @@
+const moment = require('moment')
+
 const HomePage = require('../pages/homePage')
+const PrisonerSearchPage = require('../pages/createBooking/prisonerSearchPage')
+const CourtVideoBookingsPage = require('../pages/viewBookings/courtVideoBookingsPage')
+const ManageCourtsPage = require('../pages/manageCourts/manageCourtsPage')
 
 context('A user can view the video link home page', () => {
   before(() => {
     cy.clearCookies()
     cy.task('reset')
     cy.task('stubLoginCourt', {})
+
+    Cypress.Cookies.preserveOnce('hmpps-session-dev')
+    cy.task('stubUserEmail', 'ITAG_USER')
+    cy.task('stubUser', 'ITAG_USER', 'WWI')
+
+    cy.task('stubAgencies', [{ agencyId: 'WWI', description: 'HMP Wandsworth' }])
+
+    cy.task('stubGetVideoLinkBookings', {
+      courtId: '.*?',
+      agencyId: '.*?',
+      date: moment().format('yyyy-MM-DD'),
+      bookings: [],
+    })
+
+    cy.task('stubAppointmentLocations', {
+      agency: '.*?',
+      locations: [],
+    })
+
     cy.login()
   })
 
@@ -31,7 +55,24 @@ context('A user can view the video link home page', () => {
     const homePage = HomePage.goTo()
 
     homePage.bookingTitle().contains('Book a new video link')
-
     homePage.appointmentsListTitle().contains('View and change video links')
+    homePage.manageCourtsListTitle().contains('Manage your list of courts')
+  })
+
+  it('A user is taken to the Search for a prisoner page', () => {
+    const homePage = HomePage.goTo()
+    homePage.bookingTitle().click()
+    PrisonerSearchPage.verifyOnPage()
+  })
+  it('A user is taken to the Video link bookings page', () => {
+    const homePage = HomePage.goTo()
+    homePage.appointmentsListTitle().click()
+    CourtVideoBookingsPage.verifyOnPage()
+  })
+
+  it('A user is taken to the Manage Courts page', () => {
+    const homePage = HomePage.goTo()
+    homePage.manageCourtsListTitle().click()
+    ManageCourtsPage.verifyOnPage()
   })
 })
